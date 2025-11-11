36cded8f9f870f192c96ee753b20a3e7072c6a9c
lamphnam <nampham2131@gmail.com>
2025-11-11 18:17:21 +0700

    feat: initialize Nutrilens backend with Spring Boot and Maven setup

---
+++ backend/src/test/java/com/nutrilens/nutrilens_backend/NutrilensBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nutrilens.nutrilens_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutrilensBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
