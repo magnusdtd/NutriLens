2644ddb2ac3d799155ddeb9f1decc8d835952543
magnusdtd <tdat0306@gmail.com>
2025-11-12 23:00:28 +0700

    Add Docker

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -22,4 +22,10 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+  build: {
+    outDir: 'dist', 
+    assetsDir: 'assets', 
+    emptyOutDir: true, 
+  },
+  base: './', 
 })
